0f96cee1a5ab71d34a75b15805e5e7d1c8135604
Kociub <jakub.kociubinski@gmail.com>
2014-12-06 21:31:11 +0100

    First draft from the standard asp.net mvc template

---
+++ LangBlogs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LangBlogs.Startup))]
+namespace LangBlogs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
