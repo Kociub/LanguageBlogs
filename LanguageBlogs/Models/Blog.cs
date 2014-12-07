5e6f76b614ea89d2ef07bae4824f41117c2d5d03
Kociub <jakub.kociubinski@gmail.com>
2014-12-07 13:09:42 +0100

    Blog and Post Model Classes

---
+++ LanguageBlogs/Models/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageBlogs.Models
+{
+    public class Blog
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public ICollection<string> Languages { get; set; }
+        public ICollection<Post> Posts { get; set; }
+    }
+}
---
+++ LanguageBlogs/Models/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageBlogs.Models
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Preview { get; set; }
+    }
+}
